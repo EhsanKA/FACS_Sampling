e237bd6605e882514ef8363e3916078bc56f81ca
e24kara <e.karimiara@gmail.com>
2023-12-01 20:11:00 +0100

    notebook update on sarah's data

--- notebooks/Sarah/01_AML_sampling_2M_5M_10M_aligned_non_transformed.py
+++ notebooks/Sarah/01_AML_sampling_2M_5M_10M_aligned_non_transformed.py
@@ -1,5 +1,7 @@
 import pandas as pd
-data = pd.read_csv("../../FACS_Sampling/datasets/AML_samples_and_myeloid_Aligned_non_normalized.csv")
+# data = pd.read_csv("../../FACS_Sampling/datasets/AML_samples_and_myeloid_Aligned_non_normalized.csv")
+data = pd.read_csv("FACS_Sampling/datasets/AML_samples_and_myeloid_Aligned_non_normalized.csv")
+
 
 features = ['CD33_aligned', 'HLA-DR_aligned', 'CD19 CD3_aligned',
             'CD11b_aligned', 'CD66b_aligned', 'CD16_aligned', 'CD163_aligned',
@@ -18,11 +20,11 @@ df1.to_csv("../../FACS_Sampling/datasets/AML_Aligned_non_normalized_2M.csv")
 seed = 235723
 np.random.seed(seed)
 rand_index5 = np.random.choice(data.index.values, 5000000, replace=False)
-df5 = data.iloc[rand_index1].drop(columns=["Unnamed: 0"])
+df5 = data.iloc[rand_index5].drop(columns=["Unnamed: 0"])
 df5.to_csv("../../FACS_Sampling/datasets/AML_Aligned_non_normalized_5M.csv")
 
 seed = 1646274
 np.random.seed(seed)
 rand_index10 = np.random.choice(data.index.values, 10000000, replace=False)
-df10 = data.iloc[rand_index1].drop(columns=["Unnamed: 0"])
+df10 = data.iloc[rand_index10].drop(columns=["Unnamed: 0"])
 df10.to_csv("../../FACS_Sampling/datasets/AML_Aligned_non_normalized_10M.csv")
