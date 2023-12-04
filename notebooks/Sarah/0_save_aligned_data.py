828ee55748fa0ee980c90d30958f245a1d38d547
EhsanKA <e.karimiara@gmail.com>
2023-12-04 18:03:08 +0100

    updated the data preparation code.

--- notebooks/Sarah/0_save_aligned_data.py
+++ notebooks/Sarah/0_save_aligned_data.py
@@ -1,9 +1,43 @@
 import pandas as pd
-data = pd.read_csv("../../FACS_Sampling/datasets/AML_samples_and_myeloid_cells_only_SSC_FSC.csv")
-features = ['CD33_aligned', 'HLA-DR_aligned', 'CD19 CD3_aligned',
-            'CD11b_aligned', 'CD66b_aligned', 'CD16_aligned', 'CD163_aligned',
-            'CD14_aligned', 'CD2_aligned', 'Siglec8_aligned', 'CD13_aligned',
-            'CD45_aligned', 'CD141_aligned', 'CD15_aligned', 'CD123_aligned',
-            'CD11c_aligned', 'CD117_aligned', 'CD45RA_aligned', 'CD34_aligned',
-            'ITGB7_aligned', 'CD88 CD89_aligned', 'FcER1A_aligned', 'Population']
-data[features].to_csv("../../FACS_Sampling/datasets/AML_samples_and_myeloid_Aligned_non_normalized.csv")
+import os
+
+# Environment variable for file path
+file_path_env = os.getenv('MY_FACS_DATA_PATH')
+if file_path_env is None:
+    raise ValueError("Environment variable 'MY_FACS_DATA_PATH' is not set")
+
+input_file = os.path.join(file_path_env, 'AML_samples_and_myeloid_cells_only_SSC_FSC.csv')
+if not os.path.exists(input_file):
+    raise FileNotFoundError(f"The file {input_file} does not exist")
+
+# Standardized feature names (snake_case)
+features = ['cd33_aligned', 'hla_dr_aligned', 'cd19_cd3_aligned', 'cd11b_aligned', 
+            'cd66b_aligned', 'cd16_aligned', 'cd163_aligned', 'cd14_aligned', 
+            'cd2_aligned', 'siglec8_aligned', 'cd13_aligned', 'cd45_aligned', 
+            'cd141_aligned', 'cd15_aligned', 'cd123_aligned', 'cd11c_aligned', 
+            'cd117_aligned', 'cd45ra_aligned', 'cd34_aligned', 'itgb7_aligned', 
+            'cd88_cd89_aligned', 'fcer1a_aligned', 'population']
+
+output_file = os.path.join('..', '..', 'FACS_Sampling', 'datasets', 
+                           'AML_samples_and_myeloid_aligned_non_normalized.csv')
+
+chunk_size = 10000  # Adjust based on your system's memory capacity
+first_chunk = True
+
+for chunk in pd.read_csv(input_file, chunksize=chunk_size):
+    # Standardize chunk column names
+    chunk.columns = [col.lower().replace(' ', '_') for col in chunk.columns]
+
+    # Select only the required features
+    if not set(features).issubset(set(chunk.columns)):
+        missing_features = set(features) - set(chunk.columns)
+        raise KeyError(f"These features are missing in the data: {missing_features}")
+
+    selected_data = chunk[features]
+
+    # Write the chunk to the output file
+    if first_chunk:
+        selected_data.to_csv(output_file, mode='w', index=False)
+        first_chunk = False
+    else:
+        selected_data.to_csv(output_file, mode='a', index=False, header=False)
