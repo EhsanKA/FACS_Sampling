b4b489d29cdcfb7952e48598b6c92ddc7187dbc5
EhsanKA <e.karimiara@gmail.com>
2023-12-04 19:18:51 +0100

    cleaned the 01_aml

--- notebooks/Sarah/01_AML_sampling_2M_5M_10M_aligned_non_transformed.py
+++ notebooks/Sarah/01_AML_sampling_2M_5M_10M_aligned_non_transformed.py
@@ -1,32 +1,53 @@
 import pandas as pd
-
-path = ""
-data = pd.read_csv("../../FACS_Sampling/datasets/AML_samples_and_myeloid_Aligned_non_normalized.csv")
-# data = pd.read_csv("FACS_Sampling/datasets/AML_samples_and_myeloid_Aligned_non_normalized.csv")
-
-
-features = ['CD33_aligned', 'HLA-DR_aligned', 'CD19 CD3_aligned',
-            'CD11b_aligned', 'CD66b_aligned', 'CD16_aligned', 'CD163_aligned',
-            'CD14_aligned', 'CD2_aligned', 'Siglec8_aligned', 'CD13_aligned',
-            'CD45_aligned', 'CD141_aligned', 'CD15_aligned', 'CD123_aligned',
-            'CD11c_aligned', 'CD117_aligned', 'CD45RA_aligned', 'CD34_aligned',
-            'ITGB7_aligned', 'CD88 CD89_aligned', 'FcER1A_aligned', 'Population']
-
 import numpy as np
+import os
+
+# Get the file path from the environment variable
+file_path_env = os.getenv('MY_FACS_DATA_PATH')
+
+# If the environment variable is not set, raise an error
+if file_path_env is None:
+    raise ValueError("Environment variable 'MY_FACS_DATA_PATH' is not set")
+
+# Construct the input file path
+input_file = os.path.join(file_path_env, 'datasets', 'AML_samples_and_myeloid_aligned_non_normalized.csv')
+
+# If the input file does not exist, raise an error
+if not os.path.exists(input_file):
+    raise FileNotFoundError(f"The file {input_file} does not exist")
+
+# Construct the output file paths
+output_file2 = os.path.join(file_path_env,'datasets', 'AML_data_2M.csv')
+output_file5 = os.path.join(file_path_env,'datasets', 'AML_data_5M.csv')
+output_file10 = os.path.join(file_path_env,'datasets', 'AML_data_10M.csv')
+
+# Read the input file into a pandas DataFrame
+data = pd.read_csv(input_file, low_memory=False)
+
+# Drop the unnecessary column
+data = data.drop(columns=["Unnamed: 0"])
+
+# Set the random seed for reproducibility
 seed = 235224
 np.random.seed(seed)
-rand_index2 = np.random.choice(data.index.values, 2000000, replace=False)
-df1 = data.iloc[rand_index2].drop(columns=["Unnamed: 0"])
-df1.to_csv("../../FACS_Sampling/datasets/AML_data_2M.csv")
 
+# Randomly select 2M rows from the DataFrame
+rand_index2 = np.random.choice(data.index.values, 2000000, replace=False)
+df2 = data.iloc[rand_index2]
+
+# Write the selected rows to the output file
+df2.to_csv(output_file2)
+
+# Repeat the process for 5M rows
 seed = 235723
 np.random.seed(seed)
 rand_index5 = np.random.choice(data.index.values, 5000000, replace=False)
-df5 = data.iloc[rand_index5].drop(columns=["Unnamed: 0"])
-df5.to_csv("../../FACS_Sampling/datasets/AML_data_5M.csv")
+df5 = data.iloc[rand_index5]
+df5.to_csv(output_file5)
 
+# Repeat the process for 10M rows
 seed = 1646274
 np.random.seed(seed)
 rand_index10 = np.random.choice(data.index.values, 10000000, replace=False)
-df10 = data.iloc[rand_index10].drop(columns=["Unnamed: 0"])
-df10.to_csv("../../FACS_Sampling/datasets/AML_data_10M.csv")
+df10 = data.iloc[rand_index10]
+df10.to_csv(output_file10)
