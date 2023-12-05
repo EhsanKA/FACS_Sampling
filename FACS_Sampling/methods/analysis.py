b5a644b40b294f3283a551bc7d373d579316b000
EhsanKA <e.karimiara@gmail.com>
2023-12-05 16:29:14 +0100

    more functions are cleaned!

--- FACS_Sampling/methods/analysis.py
+++ FACS_Sampling/methods/analysis.py
@@ -74,59 +74,72 @@ def get_knn_classification_report(index_subset, ref_adata, label_key='labels'):
     return df_cr, cr
 
 
-def get_mnn_classification_report(index_subset, ref_adata, label_key='labels'):
-    small_adata = ref_adata[index_subset]
-    bad_inds = small_adata.obs.index
-    bad_df = ref_adata.obs.index.isin(bad_inds)
+# def get_mnn_classification_report(index_subset, ref_adata, label_key='labels'):
+#     small_adata = ref_adata[index_subset]
+#     bad_inds = small_adata.obs.index
+#     bad_df = ref_adata.obs.index.isin(bad_inds)
 
-    X_train = small_adata.X
-    y_train = small_adata.obs[label_key]
-    X_test = ref_adata[~bad_df].X
-    y_test = ref_adata[~bad_df].obs[label_key]
+#     X_train = small_adata.X
+#     y_train = small_adata.obs[label_key]
+#     X_test = ref_adata[~bad_df].X
+#     y_test = ref_adata[~bad_df].obs[label_key]
 
-    y_pred = MNN_classifier(X_train, X_test, y_train, k=100)
+#     y_pred = MNN_classifier(X_train, X_test, y_train, k=100)
 
-    cr = classification_report(y_test, y_pred, output_dict=True)
-    crc = classification_report(y_test, y_pred)
-    df_cr = pd.DataFrame(cr)
-    return df_cr, crc
-
-
-def MNN_classifier(train_data, test_data, train_labels, k=100):
-    mutual_1, mutual_2 = find_mutual_nn(train_data, test_data, k1=k, k2=k)
-    dictionary = clean(mutual_1, mutual_2, train_labels)
-    y_pred = []
-    not_found = []
-
-    clf = neighbors.KNeighborsClassifier(n_neighbors=5)
-    clf.fit(train_data, train_labels)
-
-    for i in range(test_data.shape[0]):
-        if i in dictionary.keys():
-            label = max(set(dictionary[i]), key=dictionary[i].count)
-        else:
-            label = clf.predict(test_data[i].reshape(1, -1))[0]
-            not_found.append(i)
-        y_pred.append(label)
-    print("{} cells could not get MNN's in the other datasets.".format(len(not_found)))
-
-    return y_pred
+#     cr = classification_report(y_test, y_pred, output_dict=True)
+#     crc = classification_report(y_test, y_pred)
+#     df_cr = pd.DataFrame(cr)
+#     return df_cr, crc
 
 
-def repeat_classifier(ref_adata, label_key='labels', classifier=get_knn_classification_report, rep=5, seed=12345):
+# def MNN_classifier(train_data, test_data, train_labels, k=100):
+#     mutual_1, mutual_2 = find_mutual_nn(train_data, test_data, k1=k, k2=k)
+#     dictionary = clean(mutual_1, mutual_2, train_labels)
+#     y_pred = []
+#     not_found = []
+
+#     clf = neighbors.KNeighborsClassifier(n_neighbors=5)
+#     clf.fit(train_data, train_labels)
+
+#     for i in range(test_data.shape[0]):
+#         if i in dictionary.keys():
+#             label = max(set(dictionary[i]), key=dictionary[i].count)
+#         else:
+#             label = clf.predict(test_data[i].reshape(1, -1))[0]
+#             not_found.append(i)
+#         y_pred.append(label)
+#     print("{} cells could not get MNN's in the other datasets.".format(len(not_found)))
+
+#     return y_pred
+
+
+
+def repeat_classification(ref_adata, label_key='labels', classifier=get_knn_classification_report, rep=5, seed=12345):
+    # Set the seed for reproducibility
     np.random.seed(seed)
+
+    # Initialize lists to store the output of each method
     output_random = []
     output_fsbs = []
-    output_pbs = []
+    output_proportional = []
 
-    for i in range(rep):
-        new_seed = np.random.randint(10675)
+    # Set the maximum seed
+    MAX_SEED = 10000
 
+    # Repeat the process for a specified number of times
+    for _ in range(rep):
+        # Generate a new seed for each iteration
+        new_seed = np.random.randint(MAX_SEED)
+
+        # Generate samples using different methods
         ps, _ = bin_sample(ref_adata, n_bins=14, s_size=20, seed=new_seed)
         rs = sample_random(ref_adata, s_size=ps.size, seed=new_seed)
         prop_s, _ = proportional_sampling(ref_adata, n_bins=14, total_size=150, power=0.01, seed=new_seed)
 
+        # Apply the classifier to each method and store the output
         output_random.append(classifier(rs, ref_adata, label_key=label_key)[0])
         output_fsbs.append(classifier(ps, ref_adata, label_key=label_key)[0])
-        output_pbs.append(classifier(prop_s, ref_adata, label_key=label_key)[0])
-    return output_random, output_fsbs, output_pbs
+        output_proportional.append(classifier(prop_s, ref_adata, label_key=label_key)[0])
+
+    # Return the output of each method
+    return output_random, output_fsbs, output_proportional
